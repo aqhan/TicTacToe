7012e023019832bbb5d34986cdffe5fd7dc6aed4
Lihan Zeng <zxc1060373758@gmail.com>
2023-11-10 15:11:13 +0800

    feat: canvas画出棋盘

---
+++ ttt.ts
@@ -0,0 +1,57 @@
+interface MyApp {
+  timeOuts: number[],
+
+  initializeGame: () => void;
+  display: {
+    drawBoard: () => void;
+  }
+}
+
+var MYAPP: MyApp = (window as any).MYAPP || {
+  timeOuts: [],
+
+  initializeGame: function () {
+    MYAPP.display.drawBoard();
+  }
+}
+
+MYAPP.display = {
+  drawBoard: function (): void {
+    MYAPP.timeOuts.push(setTimeout(function () {
+      var canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+      var ctx = canvas.getContext("2d")!;
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "#000";
+
+      // vertical lines
+      ctx.beginPath();
+      ctx.moveTo(100, 0);
+      ctx.lineTo(100, 150);
+      ctx.closePath();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(200, 0);
+      ctx.lineTo(200, 150);
+      ctx.closePath();
+      ctx.stroke();
+
+      // horizontal lines
+      ctx.beginPath();
+      ctx.moveTo(0, 50);
+      ctx.lineTo(300, 50);
+      ctx.closePath();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(0, 100);
+      ctx.lineTo(300, 100);
+      ctx.closePath();
+      ctx.stroke();
+    }, 1500));
+  }
+}
+
+$(function () {
+  MYAPP.initializeGame();
+});
